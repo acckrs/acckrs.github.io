6b035ba1a14aed882739d1ac41cc307b973abcec
Aleksandar Krstić <acckrs@gmail.com>
2016-03-17 14:27:22 +0100

    upload-js-and-css

---
+++ js/acckrs.js
@@ -0,0 +1,10 @@
+$("#text1").css("color", "#cc3300");
+$("#text2").addClass("animated fadeInDown"); 
+$("#about").addClass("lead well");
+$("#contact").addClass("h3 well");
+$("#contact_text").addClass("well col-xs-12");
+$("#quote_text").addClass("blockquote well col-xs-12");
+$("#footer_text").addClass("row text-center");
+
+
+
