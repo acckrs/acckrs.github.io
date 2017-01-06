71a5d192e8ef4db33f06be69c9f72a779f382894
Aleksandar Krstić <acckrs@gmail.com>
2017-01-06 16:32:27 +0100

    put jquery script in acckrs.js file

--- js/acckrs.js
+++ js/acckrs.js
@@ -6,5 +6,30 @@ $("#contact_text").addClass("well col-xs-12");
 $("#quote_text").addClass("blockquote well col-xs-12");
 $("#footer_text").addClass("row text-center");
 
+$(document).ready(function () {
+    function randomQuote() {
+        $.ajax({
+            url: "http://api.forismatic.com/api/1.0/?",
+            dataType: "jsonp",
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+            success: function (response) {
+                $("#random-quote").html("<p id='random-quote'>" +
+                  response.quoteText + "</p>");
+
+                $("#quoteAuthor").html("<p id='quoteAuthor'>" + response.quoteAuthor + " </p>");
+                $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
+                  ' (' + response.quoteAuthor + ')');
+            }
+        });
+    }
+    $(function () {
+        randomQuote();
+    });
+
+    $("#getMessage").click(function () {
+        randomQuote();
+    });
+});
+
 
 
