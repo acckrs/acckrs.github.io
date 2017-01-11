fdb75b9b790908dc74557d5c432b199352f27592
Aleksandar Krstić <acckrs@gmail.com>
2017-01-11 11:51:54 +0100

    k

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -26,7 +26,7 @@ function loadData() {
     }).done(function (result) {
         var snippets = [];
         $.each(result.response.docs, function (val) {
-            snippets.push([val]);
+            snippets.push([val.toString()]);
         });
         for (var i = 0; i < snippets.length; i++) {
             console.log(snippets[i]);
