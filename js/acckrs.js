7590df9c9685bc423f94ae62473d3fea3a8e54e2
Aleksandar Krstić <acckrs@gmail.com>
2017-01-12 21:03:36 +0100

    s

--- js/acckrs.js
+++ js/acckrs.js
@@ -1,15 +1,15 @@
 
-    function randomQuote() {
+function randomQuote() {
+    
 
-
-        $.ajax({
-            url: "http://api.forismatic.com/api/1.0/?",
-            dataType: "JSONP",
-            data: "method=getQuote&format=jsonp&lang=en&callback=quote",
-        }).done(function(reponse){
-            console.log(response);
-            });
-    }
+    $.ajax({
+        url: "http://api.forismatic.com/api/1.0/?",
+        dataType: "JSONP",
+        data: "method=getQuote&format=jsonp&lang=en&callback=quote",
+    }).done(function (reponse) {
+        console.log(response);
+    });
+};
    
     $("#getMessage").click(randomQuote);
     
