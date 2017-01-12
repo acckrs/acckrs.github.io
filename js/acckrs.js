b627e1648f6e90f960164a6e44b3497a93519f35
Aleksandar Krstić <acckrs@gmail.com>
2017-01-12 21:28:02 +0100

    sd

--- js/acckrs.js
+++ js/acckrs.js
@@ -1,17 +1,19 @@
 
-function randomQuote() {
-    
+$(document).ready(function () {
+    function randomQuote() {
 
-    $.ajax({
-        url: "http://api.forismatic.com/api/1.0/?",
-        dataType: "JSONP",
-        data: "method=getQuote&format=jsonp&lang=en&callback=quote",
-    }).done(function (reponse) {
-        console.log(response);
-    });
-};
-   
-    $("#getMessage").on("click",randomQuote);
+
+        $.ajax({
+            url: "http://api.forismatic.com/api/1.0/?",
+            
+            data: "method=getQuote&format=jsonp&lang=en",
+        }).done(function (reponse) {
+            console.log(response);
+        });
+    };
+
+    $("#getMessage").on("click", randomQuote);
+});
     
 
 
