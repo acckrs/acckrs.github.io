b899bc01392c3294ebb93cad136e7e6a4bd7bb9d
Aleksandar Krstić <acckrs@gmail.com>
2017-01-12 21:40:32 +0100

    d

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -64,10 +64,7 @@ function loadData() {
             $("#wikipedia-links").append('</ul>');
         }
         clearTimeout(wikiRequestTimeout);
-    })//.fail(function () {
-       //$("#wikipedia-header").text('Failed to get Wikipedia links about ' + titleCase($city.toString()) + ':');
-    //})
-    ;
+    });
    
     $greeting.text('So you want to live in '+ $("#street").val() + ',' + $("#city").val()+'?').css("color","white")
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $address + '&key=AIzaSyAXt_KiHM2AXMqnwRCMHSYmjIbb6wgu5BU">')
