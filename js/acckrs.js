e570d3979d24ac2a46cc869eddbefcba939e5267
Aleksandar Krstić <acckrs@gmail.com>
2017-01-12 22:48:19 +0100

    as

--- js/acckrs.js
+++ js/acckrs.js
@@ -15,12 +15,32 @@ $(document).ready(function () {
         }).done(function (response) {
             $("#quoteText").text(response.quoteText);
             $("#quoteAuthor").text(response.quoteAuthor);
+            $("#tweetQuote").attr("href", "http://twitter.com/home/?status=" + response.quoteText + '(' + response.quoteAuthor + ')');
             console.log(response.quoteText.toString());
             //
         });
     };
 
-    $("#getMessage").on("click", randomQuote);
+   /* function tweetQuote() {
+        var $url = "http://api.forismatic.com/api/1.0/";
+        $url += '?&' + $.param({
+            'method': 'getQuote',
+            'format': 'jsonp',
+            'lang': 'en'
+
+        }) + '&jsonp=?';
+
+        $.ajax({
+            url: $url,
+            dataType: "JSONP"
+        }).done(function (response) {
+           
+            console.log(response.quoteText.toString());
+         });
+    };*/
+
+    $("#getNewQuote").on("click", randomQuote);
+  //  $("#tweetQuote").on("click", tweetQuote);
 });
     
 
