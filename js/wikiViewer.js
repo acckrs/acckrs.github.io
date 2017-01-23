1e181ab81536c0c61525dd66be654d71fad1caa6
Aleksandar Krstić <acckrs@gmail.com>
2017-01-23 23:47:16 +0100

    7y

--- js/wikiViewer.js
+++ js/wikiViewer.js
@@ -1,35 +1,40 @@
+$(document).ready(function () {
+    $("#submit-btn").click(function () {
+        var $searchString = $("#searchText").val();
+        var $wikiElem = $("#wikipedia-links");
+        var $wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=info&inprop=url&callback=wikiCallback&search=text";
 
-function searchWiki() {
-    var $searchString = $("#searchText").val();
-    var $wikiElem = $("#wikipedia-links");
-    var $wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=info&inprop=url&callback=wikiCallback&search=text";
-
-    // clear out old data before new request
-     $wikiElem.text("");
+        // clear out old data before new request
+        $wikiElem.text("");
 
 
 
-    console.log($searchString);
-    $.ajax({
-        url: $wikiUrl,
-        dataType: "JSONP"
-    }).done(function (result) {
-        console.log($wikiUrl);
-        console.log(result);
         console.log($searchString);
-        console.log("Nista");
-        $("#wikipedia-links").html('<ul class="well" id="wikipedia-links">');
-        for (var j = 0; j < result[3].length; j++) {
-            $("#wikipedia-links").append('<li><a href="' + result[3][j] + '">' +
-                                            result[1][j] + '</a><p>' +
-                                            result[2][j] + '</p>'
-                );
-            $("#wikipedia-links").append('</ul>');
-        }
-        
-    });
+        $.ajax({
+            url: $wikiUrl,
+            dataType: "JSONP"
+        }).done(function (result) {
+            console.log($wikiUrl);
+            console.log(result);
+            console.log($searchString);
+            console.log("Nista");
+            $("#wikipedia-links").html('<ul id="wikipedia-links">');
+            for (var j = 0; j < result[3].length; j++) {
+                $("#wikipedia-links").append('<li><a href="' + result[3][j] + '">' +
+                                                result[1][j] + '</a><p>' +
+                                                result[2][j] + '</p>'
+                    );
+                $("#wikipedia-links").append('</ul>');
+            }
 
-    return false;
-};
+        });
 
-$('#submit-btn').click(searchWiki());
+        return false;
+    })
+
+
+    
+
+  
+
+})
