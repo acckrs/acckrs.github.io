d8ef6d6ad9aabcf45bc0261791f5b04340349a4f
Aleksandar Krstić <acckrs@gmail.com>
2017-01-23 22:34:51 +0100

    test

--- wikipedia_viewer/js/wikiViewer.js
+++ wikipedia_viewer/js/wikiViewer.js
@@ -1,8 +1,8 @@
 
 function searchWiki() {
-
+    var $searchString = $("#searchText");
     var $wikiElem=$("#wikipedia-links");
-    var $wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=info&inprop=url&callback=wikiCallback&search=" + $city;
+    var $wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=info&inprop=url&callback=wikiCallback&search=" + $searchString;
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -38,4 +38,4 @@ function searchWiki() {
     return false;
 };
 
-$('#form-container').submit(searchWiki);
+$('#search-input').submit(searchWiki);
