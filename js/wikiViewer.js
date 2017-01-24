4345293d187b444a24ed62e534f05044a3b50e5c
Aleksandar Krstić <acckrs@gmail.com>
2017-01-24 09:50:56 +0100

    d

--- js/wikiViewer.js
+++ js/wikiViewer.js
@@ -29,6 +29,24 @@ $(document).ready(function () {
 
         });
 
+        $("#searchText").autocomplete({
+            source: function (request, response) {
+                console.log(request.term);
+                $.ajax({
+                    url: "http://en.wikipedia.org/w/api.php",
+                    dataType: "jsonp",
+                    data: {
+                        'action': "opensearch",
+                        'format': "json",
+                        'search': request.term
+                    },
+                    success: function (data) {
+                        response(data[1]);
+                    }
+                });
+            }
+        });
+
         return false;
     })
 
