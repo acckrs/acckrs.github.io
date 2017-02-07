c019050daf1a72d05dd0d39d9519139163ddf208
akrstic <akrstic@W-CS-WS314>
2017-02-07 14:16:48 +0100

    w

--- eloquentJS/weresquirrel.js
+++ eloquentJS/weresquirrel.js
@@ -109,8 +109,8 @@ function phi(table) {
             (table[0] + table[2]));
 }
 
-//function for creating distribution table for specific event 
-function hasItem(event, entry) {
+//functions for creating distribution table for specific event 
+function hasEvent(event, entry) {
     return entry.events.indexOf(event) != -1;
 }
 
@@ -118,27 +118,62 @@ function tableFor(event, journal) {
     table = [0, 0, 0, 0];
     for (var i = 0; i < journal.length; i++) {
         var index = 0;
-        if (hasItem(event, journal[i])) index += 1;
+        if (hasEvent(event, journal[i])) index += 1;
         if (journal[i].squirrel) index += 2;
         table[index] += 1;
     }
     return table;
 }
 
+//
 var map = {};
 
 function storePhi(event, phi) {
     map[event] = phi;
 }
 
-
-
-
-
-for (var event in map) {
-    console.log(event + '    ,' + map[event]);
+function gatherCorrelations(journal) {
+    var phis = {};
+    for (var entry = 0; entry < journal.length; entry++) {
+        var events = journal[entry].events;
+        for (var i = 0; i < events.length; i++) {
+            var event = events[i];
+            if (!(event in phis))
+                phis[event] = phi(tableFor(event, journal));
+        }
+    }
+    return phis;
 }
 
+var correlations = gatherCorrelations(JOURNAL);
+
+for (var event in correlations) {
+    storePhi(event)
+    if (correlations[event] > 0.1 || correlations[event] < -0.1)
+        console.log(event + ': ' + correlations[event]);
+}
+
+for (var i = 0; i < JOURNAL.length; i++) {
+    var entry = JOURNAL[i];
+    if (hasEvent("peanuts", entry) &&
+        !hasEvent("brushed teeth", entry))
+        entry.events.push("peanut teeth");
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
+/*
+for (var i = 0; i < JOURNAL.length; i++) {
+    var entry = JOURNAL[i];
+    if (hasEvent("peanuts", entry) &&
+        !hasEvent("brushed teeth", entry))
+        entry.events.push("peanut teeth");
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
+
+*/
+/*for (var event in map) {
+    console.log(event + '    ,' + map[event]);
+}
+*/
 
 //console.log(map);
 //console.log(tableFor("brushed teeth", JOURNAL));
