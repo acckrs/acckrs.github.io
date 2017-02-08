c4d992f8e2d432fb8a1899f7ff2fb7426a88713b
Aleksandar Krstić <acckrs@gmail.com>
2017-02-08 12:00:38 +0100

    s

--- eloquentJS/chapter 4/exercises/list.js
+++ eloquentJS/chapter 4/exercises/list.js
@@ -1,5 +1,44 @@
-function arrayToList(arr){
-    var list ={};
-    
+//pretvori niz u listu
+function arrayToList(arr) {
+    var list = null;
+    for (var i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
+            rest: list
+        };
+    }
+    return list;
+};
 
+//pretvori listu u niz
+function listToArray(list) {
+    var arr = [];
+    for(var node=list;node;node=node.rest){
+        arr.push(node.value);
+    }
+    return arr;
 }
+
+//dodaj element u listu
+function prepend(element,list){
+    var newArr=listToArray(list).unshift(element);
+    return newArr;
+}
+
+
+
+var list = {
+    "value": 1,
+    "rest": {
+        "value": 2,
+        "rest": {
+            "value": 3,
+            "rest": null
+        }
+    }
+};
+console.log(prepend(4,list));
+
+var testArr = [10, 20, 30];
+console.log(arrayToList(testArr));
+console.log(listToArray(arrayToList(testArr)));
