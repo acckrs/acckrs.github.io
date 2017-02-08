c218ee6eb3bcc24a1777a4af1f08d8ae1c523dda
Aleksandar Krstić <acckrs@gmail.com>
2017-02-08 10:54:03 +0100

    e

--- eloquentJS/chapter 4/exercises/reversedArray.js
+++ eloquentJS/chapter 4/exercises/reversedArray.js
@@ -6,4 +6,22 @@ function reverseArray(arr) {
 
     return revArr;
 }
-console.log(reverseArray(["A", "B", "C"]));
+
+function reverseArrayInPlace(arrayValue){
+    len=arrayValue.length-1;
+    for (var i=0;i<=len/2;i++){
+        var inter;
+        inter=arrayValue[i];
+        arrayValue[i]=arrayValue[len-i];
+        arrayValue[len-i]=inter;
+
+    }
+return arrayValue;
+
+}
+
+var arrayValue=["a","b","c","d"];
+console.log(arrayValue);
+console.log(reverseArrayInPlace(arrayValue));
+console.log(arrayValue);
+//console.log(reverseArray(arrayValue));
