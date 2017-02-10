bee48c81b7f95942c9e2398457fef8942b575e50
akrstic <akrstic@W-CS-WS314>
2017-02-10 16:42:55 +0100

    r

--- TwichTV/twich.js
+++ TwichTV/twich.js
@@ -1,10 +1,12 @@
-function checkTwich() {
-    $url = "https://api.twitch.tv/kraken/streams/freecodecamp?callback=?;"
-    $.ajax({
-        url: $url,
-        dataType: "JSONP",
-        type: "GET"
-    }).done(function(response) {
-        console.log(data);
-    })
-};
+$(document).ready(function() {
+    function checkTwich() {
+        $url = "https://api.twitch.tv/kraken/streams/freecodecamp?callback=?;"
+        $.ajax({
+            url: $url,
+            dataType: "JSONP",
+            type: "GET"
+        }).done(function(response) {
+            console.log(data);
+        })
+    };
+});
