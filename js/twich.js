03bbf9a2203bf52cd3c6e9265fde1e486f4915eb
akrstic <akrstic@W-CS-WS314>
2017-02-11 18:22:21 +0100

    s

--- js/twich.js
+++ js/twich.js
@@ -102,13 +102,13 @@ var streams = [{
 ]
 $(document).ready(function() {
     function checkTwich(val) {
-        $url = streams;
+        $url = "https://wind-bow.gomix.me/twitch-api/channels/freecodecamp";
         $.ajax({
             url: $url,
             dataType: "JSONP",
             type: "GET"
         }).done(function(response) {
-
+            $("#twichLogo").html('<div id="twichLogo"><img src="' + response.logo + '"/></div>')
             return console.log(response);
 
         })
