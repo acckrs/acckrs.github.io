25e41fad9faa80ee4dbd33239838b8fa7fcaccca
akrstic <akrstic@W-CS-WS314>
2017-02-11 21:33:54 +0100

    s

--- js/twich.js
+++ js/twich.js
@@ -14,14 +14,12 @@ $(document).ready(function() {
                 if (!response.stream) {
                     return console.log(response._links.channel + " is offline")
                 } else {
-                    //$('#twichTable >tbody:last-child').append('<tr><td><img class="img-thumbnail img-responsive" src="' + response.stream.channel.logo + '"/></td><td>text</td><td>text2</td></tr>');
+                    $("#twichLogo").html('<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.stream.channel.logo + '"/></div>')
                     $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.display_name + '</a></div>')
                     return console.log(response);
                 }
             })
         };
     }
-
-    function check
     $("#testTwich").on("click", checkTwich);
 });
