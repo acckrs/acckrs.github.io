4bda404689fa43d2acde04eea58a8c1e99fed3ec
akrstic <akrstic@W-CS-WS314>
2017-02-11 18:47:51 +0100

    w

--- js/twich.js
+++ js/twich.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
             dataType: "JSONP",
             type: "GET"
         }).done(function(response) {
-            $("#twichLogo").html('<div id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
+            $("#twichLogo").html('<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
+            $("#twichLink").html('<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.url + '" role="button">' + response.name + '</a></div>')
             return console.log(response);
 
         })
