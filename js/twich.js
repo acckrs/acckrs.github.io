42267b4ca7ea7692ee596062707c60f1b1a5ab6a
akrstic <akrstic@W-CS-WS314>
2017-02-11 22:28:06 +0100

    w

--- js/twich.js
+++ js/twich.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
                     return console.log(response._links.channel + " is offline")
                 } else {
                     $("#twichTable > tbody:last-child").append('<tr>' +
-                        '<td><img class="img-thumbnail img-responsive" src="' + response.stream.channel.logo + '"/></td>' +
+                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" src="' + response.stream.channel.logo + '"/></td>' +
                         '<td>' + response.stream.channel.display_name + '</td>' +
-                        '<td>Text3</td>' +
+                        '<td><a href src="' + response.stream.channel.url + '"/>' + response.stream.channel.url + '</td>' +
                         '</tr>');
                     $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.display_name + '</a></div>')
                     return console.log(response);
