9ff7fa6ea7378ef23d712f6fb92528d06bdb6889
akrstic <akrstic@W-CS-WS314>
2017-02-11 20:35:38 +0100

    d

--- js/twich.js
+++ js/twich.js
@@ -11,12 +11,13 @@ $(document).ready(function() {
                 dataType: "JSONP",
                 type: "GET"
             }).done(function(response) {
-
-
-                $("#twichLogo").html('<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
-                $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.url + '">' + response.name + '</a></div>')
-                return console.log(response);
-
+                if (!response.stream) {
+                    return console.log(response._links.channel + " is offline")
+                } else {
+                    $("#twichLogo").html('<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
+                    $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.url + '">' + response.name + '</a></div>')
+                    return console.log(response);
+                }
             })
         };
     }
