67ce62db6d89e33147e9ab3193da03d66253adc0
akrstic <akrstic@W-CS-WS314>
2017-02-11 20:27:45 +0100

    s

--- js/twich.js
+++ js/twich.js
@@ -1,16 +1,23 @@
+var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+
 $(document).ready(function() {
     function checkTwich() {
-        $url = "https://wind-bow.gomix.me/twitch-api/channels/freecodecamp";
-        $.ajax({
-            url: $url,
-            dataType: "JSONP",
-            type: "GET"
-        }).done(function(response) {
-            $("#twichLogo").html('<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
-            $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.url + '">' + response.name + '</a></div>')
-            return console.log(response);
+        for (var i = 0; i < streams.length; i++) {
 
-        })
-    };
+            $.ajax({
+                url: $url + streams[i],
+                dataType: "JSONP",
+                type: "GET"
+            }).done(function(response) {
+
+
+                $("#twichLogo").html('<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" id="twichLogo"><img class="img-thumbnail img-responsive" src="' + response.logo + '"/></div>')
+                $("#twichLink").html('<div class="animated fadeInDown col-xs-8 col-sm-8 col-md-8 col-lg-8" id="twichLink"><a class="btn btn-large btn-block btn-default" href="' + response.url + '">' + response.name + '</a></div>')
+                return console.log(response);
+
+            })
+        };
+    }
     $("#testTwich").on("click", checkTwich);
 });
