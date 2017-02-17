8121ace46a448667d1e704944c7d5b32f5331e74
akrstic <akrstic@W-CS-WS314>
2017-02-17 16:46:07 +0100

    e

--- js/twich.js
+++ js/twich.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
                     $("#twichTable > tbody:last-child").append('<tr>' +
                         '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
                         '<td>' + response.stream.channel.display_name + '</td>' +
-                        '<td><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.url + '</a></td>' +
+                        '<td><a class="btn btn-large btn-block btn-default" href="' + replaceText(response.stream.channel.url) + '">' + replaceText(response.stream.channel.url) + '</a></td>' +
                         '<td>Online</td>' +
                         '</tr>');
 
