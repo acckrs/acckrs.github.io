28f541944e8bf314e364bbf8618a9ad1a58cf1c2
akrstic <akrstic@W-CS-WS314>
2017-02-17 13:56:00 +0100

    d

--- js/twich.js
+++ js/twich.js
@@ -3,6 +3,8 @@ $url = "https://wind-bow.gomix.me/twitch-api/streams/";
 
 
 $(document).ready(function() {
+
+
     function checkOnlineTwiches() {
         $("#twichTableBody").html('<tbody id="twichTableBody"><tr><td></td><td></td><td></td></tr></tbody>');
         for (var i = 0; i < streams.length; i++) {
@@ -14,30 +16,6 @@ $(document).ready(function() {
             }).done(function(response) {
                 if (!response.stream) {
                     return console.log(response + " is offline")
-                } else {
-                    $("#twichTable > tbody:last-child").append('<tr>' +
-                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
-                        '<td>' + response.stream.channel.display_name + '</td>' +
-                        '<td><a href class="btn btn-large btn-block btn-default" src="' + response.stream.channel.url + '">' + response.stream.channel.url + '</a></td>' +
-                        '</tr>');
-
-                    return console.log(response);
-                }
-            })
-        };
-    }
-
-    function checkOnlineTwiches() {
-        $("#twichTableBody").html('<tbody id="twichTableBody"><tr><td></td><td></td><td></td></tr></tbody>');
-        for (var i = 0; i < streams.length; i++) {
-
-            $.ajax({
-                url: $url + streams[i],
-                dataType: "JSONP",
-                type: "GET"
-            }).done(function(response) {
-                if (!response.stream) {
-                    return console.log(response._links.channel + " is offline")
                 } else {
                     $("#twichTable > tbody:last-child").append('<tr>' +
                         '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
