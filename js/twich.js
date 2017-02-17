3c6a0ec3240183936b932d654731cac098bc636f
akrstic <akrstic@W-CS-WS314>
2017-02-17 17:51:59 +0100

    e

--- js/twich.js
+++ js/twich.js
@@ -9,6 +9,55 @@ $(document).ready(function() {
         return newStr;
     };
 
+    function checkFCCTwich() {
+        $('#twichTable').html('<table item-width="70%" id="twichTable" class="table">' +
+            '<thead>' +
+            '<tr>' +
+            '<th>Channel logo</th>' +
+            '<th>Channel name</th>' +
+            '<th>Channel link</th>' +
+            '<th>Channel status</th>' +
+            '</tr>' +
+            '</thead>' +
+            '<tbody id="twichTableBody">' +
+            '<tr>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '</tr>' +
+            '</tbody>' +
+            '</table>');
+        $("#twichTableBody").html('<tbody id="twichTableBody"><tr><td><td></td></td><td></td><td></td></tr></tbody>');
+        for (var i = 0; i < streams.length; i++) {
+
+            $.ajax({
+                url: $url + streams[i],
+                dataType: "JSONP",
+                type: "GET"
+            }).done(function(response) {
+                if (!response.stream) {
+                    $("#twichTable > tbody:last-child").append('<tr>' +
+                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="/img/freecodecamp-profile_image-d9514f2df0962329-300x300.png"/></td>' +
+                        '<td>FreeCodeCamp</td>' +
+                        '<td><a class="btn btn-large btn-block btn-default" href="http://www.twitch.tv/freecodecamp">http://www.twitch.tv/freecodecamp</a></td>' +
+                        '<td>Offline</td>' +
+                        '</tr>');
+                    return console.log(response._links.channel + ' is offline')
+                } else {
+                    $("#twichTable > tbody:last-child").append('<tr>' +
+                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
+                        '<td>' + response.stream.channel.display_name + '</td>' +
+                        '<td><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.url + '</a></td>' +
+                        '<td>Online</td>' +
+                        '</tr>');
+
+                    return console.log(response.stream.channel.display_name + ' is online');
+                }
+            })
+        };
+    }
+
     function checkOnlineTwiches() {
         $('#twichTable').html('<table item-width="70%" id="twichTable" class="table">' +
             '<thead>' +
@@ -92,4 +141,5 @@ $(document).ready(function() {
 
     $("#onlineTwich").on("click", checkOnlineTwiches);
     $("#offlineTwich").on("click", checkOfflineTwiches);
+    $("#testFCC").on("click", checkFCCTwich);
 });
