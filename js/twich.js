8c8b7f8dd5a50b028e1d0d4f9f511e81e0b498cd
akrstic <akrstic@W-CS-WS314>
2017-02-17 17:58:24 +0100

    d

--- js/twich.js
+++ js/twich.js
@@ -29,33 +29,31 @@ $(document).ready(function() {
             '</tbody>' +
             '</table>');
         $("#twichTableBody").html('<tbody id="twichTableBody"><tr><td><td></td></td><td></td><td></td></tr></tbody>');
-        for (var i = 0; i < streams.length; i++) {
+        $.ajax({
+            url: $url + 'freecodecamp',
+            dataType: "JSONP",
+            type: "GET"
+        }).done(function(response) {
+            if (!response.stream) {
+                $("#twichTable > tbody:last-child").append('<tr>' +
+                    '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="/img/freecodecamp-profile_image-d9514f2df0962329-300x300.png"/></td>' +
+                    '<td>FreeCodeCamp</td>' +
+                    '<td><a class="btn btn-large btn-block btn-default" href="http://www.twitch.tv/freecodecamp">http://www.twitch.tv/freecodecamp</a></td>' +
+                    '<td>Offline</td>' +
+                    '</tr>');
+                return console.log(response._links.channel + ' is offline')
+            } else {
+                $("#twichTable > tbody:last-child").append('<tr>' +
+                    '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
+                    '<td>' + response.stream.channel.display_name + '</td>' +
+                    '<td><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.url + '</a></td>' +
+                    '<td>Online</td>' +
+                    '</tr>');
 
-            $.ajax({
-                url: $url + streams[i],
-                dataType: "JSONP",
-                type: "GET"
-            }).done(function(response) {
-                if (!response.stream) {
-                    $("#twichTable > tbody:last-child").append('<tr>' +
-                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="/img/freecodecamp-profile_image-d9514f2df0962329-300x300.png"/></td>' +
-                        '<td>FreeCodeCamp</td>' +
-                        '<td><a class="btn btn-large btn-block btn-default" href="http://www.twitch.tv/freecodecamp">http://www.twitch.tv/freecodecamp</a></td>' +
-                        '<td>Offline</td>' +
-                        '</tr>');
-                    return console.log(response._links.channel + ' is offline')
-                } else {
-                    $("#twichTable > tbody:last-child").append('<tr>' +
-                        '<td><img class="animated fadeInDown img-thumbnail img-responsive" style="width: 10%; height: 10%" src="' + response.stream.channel.logo + '"/></td>' +
-                        '<td>' + response.stream.channel.display_name + '</td>' +
-                        '<td><a class="btn btn-large btn-block btn-default" href="' + response.stream.channel.url + '">' + response.stream.channel.url + '</a></td>' +
-                        '<td>Online</td>' +
-                        '</tr>');
+                return console.log(response.stream.channel.display_name + ' is online');
+            }
+        })
 
-                    return console.log(response.stream.channel.display_name + ' is online');
-                }
-            })
-        };
     }
 
     function checkOnlineTwiches() {
